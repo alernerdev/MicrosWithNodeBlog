e51062acc1e691c778ef80824cfa29e10eb06f6b
Alex Lerner <alernerdev@gmail.com>
2020-08-30 14:59:16 -0400

    added store to event bus so that services can ask for whatever they have missed

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -5,7 +5,17 @@ import React from 'react';
 export default ({comments}) => {
 
      const renderedcomments = comments.map(comment => {
-        return <li key = {comment.id}>{comment.content}</li>;
+        let content;
+        console.log(comment);
+        if (comment.status === 'approved') {
+            content = comment.content;
+        } else if (comment.status === 'pending') {
+            content = 'Comment is awaiting moderation';
+        } else if (comment.status === 'rejected') {
+            content = 'Comment has been rejected';
+        }
+
+        return <li key = {comment.id}>{content}</li>;
     });
 
     return <ul>
